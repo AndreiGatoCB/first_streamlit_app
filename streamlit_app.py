4e70d68e9eb9db6a592da2ceb763bf27b69bc975
Brandon Albornoz <98832349+AndreiGatoCB@users.noreply.github.com>
2023-07-07 13:54:31 -0300

    Update streamlit_app.py
    Add emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My parents new healthy dinner.')
 
 streamlit.header('Breakfast menu.')
-streamlit.text('Omega 3 & Blueberry Oatmeal.')
-streamlit.text('Kale, spinach & rocker smoothie.')
-streamlit.text('Hard-boiled free-range egg.')
+streamlit.text(' 🥣  Omega 3 & Blueberry Oatmeal.')
+streamlit.text(' 🥗  Kale, spinach & rocker smoothie.')
+streamlit.text(' 🐔  Hard-boiled free-range egg.')
+streamlit.text(' 🥑🍞  Avocado toast.')
